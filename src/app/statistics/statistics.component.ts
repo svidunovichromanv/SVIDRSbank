088843817be5180a9932dcfac18f08e06968e57c
Roman <32240494+svidunovichromanv@users.noreply.github.com>
2018-12-14 19:08:59 +0300

    chart of budget try line

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -11,21 +11,38 @@ export class StatisticsComponent implements OnInit {
   private dataShow: string;
   private data: Array<Idata> = [];
   public dataTablePieChartBudget: Array<Array<string|number>>;
-  public pieChartData: any /*=  {
-    chartType: 'PieChart',
-    dataTable: this.dataTablePieChartBudget,
-    options: {'title': 'pieChartData'},
-  }*/;
+  public pieChartDataStructureBudget: any;
+  public pieChartDataBudget: any = {
+    chartType: 'LineChart',
+    dataTable: [
+      [new Date(2015, 0, 1), 5],  [new Date(2015, 0, 2), 7],  [new Date(2015, 0, 3), 3],
+      [new Date(2015, 0, 4), 1],  [new Date(2015, 0, 5), 3],  [new Date(2015, 0, 6), 4],
+      [new Date(2015, 0, 7), 3],  [new Date(2015, 0, 8), 4],  [new Date(2015, 0, 9), 2],
+      [new Date(2015, 0, 10), 5], [new Date(2015, 0, 11), 8], [new Date(2015, 0, 12), 6],
+      [new Date(2015, 0, 13), 3], [new Date(2015, 0, 14), 3], [new Date(2015, 0, 15), 5],
+      [new Date(2015, 0, 16), 7], [new Date(2015, 0, 17), 6], [new Date(2015, 0, 18), 6],
+      [new Date(2015, 0, 19), 3], [new Date(2015, 0, 20), 1], [new Date(2015, 0, 21), 2],
+      [new Date(2015, 0, 22), 4], [new Date(2015, 0, 23), 6], [new Date(2015, 0, 24), 5],
+      [new Date(2015, 0, 25), 9], [new Date(2015, 0, 26), 4], [new Date(2015, 0, 27), 9],
+      [new Date(2015, 0, 28), 8], [new Date(2015, 0, 29), 6], [new Date(2015, 0, 30), 4],
+      [new Date(2015, 0, 31), 6], [new Date(2015, 1, 1), 7],  [new Date(2015, 1, 2), 9]
+    ],
+    options: {
+      'title': 'Бюджет ',
+      width: 500,
+      height: 500,
+    },
+  };
   constructor(private datasource: SvidrsBankDatasource) {
     datasource.getOneMonth(2019, 0). subscribe((data) => {
       this.data.push(...data);
-      this.updatedataTablePieChartBudget();
+      this.updatedataChart();
     });
   }
 
   ngOnInit() {
   }
-  updatedataTablePieChartBudget(): void {
+  updatedataChart(): void {
     this.dataTablePieChartBudget = [];
     this.dataTablePieChartBudget.push(['Бюджет', 'Budget all']);
     let summFood = 0;
@@ -45,16 +62,15 @@ export class StatisticsComponent implements OnInit {
     this.dataTablePieChartBudget.push(['Одежда', summDress]);
     this.dataTablePieChartBudget.push(['Питомец', summPet]);
     this.dataTablePieChartBudget.push(['Транспорт', summTransport]);
-    console.log(this.dataTablePieChartBudget);
-
-    setTimeout(() => {
-      this.pieChartData =  {
+    this.pieChartDataStructureBudget =  {
         chartType: 'PieChart',
         dataTable: this.dataTablePieChartBudget,
-        options: {'title': 'pieChartData'},
+        options: {
+          'title': 'Бюджет ',
+          width: 500,
+          height: 500,
+        },
       };
-      console.log(this.pieChartData);
-    }, 0);
   }
 
   getdataShow(): string {
@@ -64,14 +80,14 @@ export class StatisticsComponent implements OnInit {
     if (evn === 'Все') {
       return this.datasource.getAllData(). subscribe((data) => {
         this.data = data;
-        this.updatedataTablePieChartBudget();
+        this.updatedataChart();
       });
     } else {
       const year: number = +evn.slice(0, 4);
       const month: number = +evn.slice(5, 7) - 1;
       return this.datasource.getOneMonth(year, month). subscribe((data) => {
         this.data = data;
-        this.updatedataTablePieChartBudget();
+        this.updatedataChart();
       });
     }
     console.log(evn);
